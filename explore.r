0ec9a2f9dde5244fc40c771d6c40b084ee0d3e69
Thomas Levine <_@thomaslevine.com>
2015-09-01 20:39:59 -0400

    j

--- explore.r
+++ explore.r
@@ -5,16 +5,16 @@ animals <- sqldf('select * from incidents', dbname = 'animals.db')
 # Remove things that are the same for all rows or all but one row.
 for (colname in names(animals)) {
   n <- length(unique(animals[,colname]))
-  if (n <= 2)
+  if (n <= 2) {
+    print(colname)
     animals[,colname] <- NULL
+  }
 }
 
-'
-names.thing <- c("animal", "quantity", "body_part_found",
-names.date <- c("date_started", "date_closed"
-names.reporting <- c("source", "division"
-, "status", "complaint_details", "park_or_facility"
-[11] "property_number", "park_district", "additional_location_details" "council_district_number", "site_street_address"
-[16] "site_borough", "site_city_zip", "lat", "lng", "resolution_action_updated"
-[21] "resolution_description", "time_to_action"
-'
+names.thing <- c("animal", "quantity", "body_part_found", "complaint_details", "resolution_description")
+names.date <- c("date_started", "date_closed", "resolution_action_updated")
+names.reporting <- c("source", "division")
+names.location <- c("park_or_facility", "property_number", "park_district",
+                    "additional_location_details", "council_district_number",
+                    "site_street_address", "site_borough", "site_city_zip",
+                    "lat", "lng")
